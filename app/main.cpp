4c4e0cb40785817e83f6375b3b9e10205dc90968
Jan Merwinski <252959@student.pwr.edu.pl>
2021-06-08 21:01:28 +0200

    dodane proste menu

--- app/main.cpp
+++ app/main.cpp
@@ -13,135 +13,27 @@
 #include <string>
 
 #include "Wektor.hh"
+#include "Wektor3D.hh"
 #include "Macierz.hh"
+#include "Macierz3x3.hh"
+#include "Prostopadloscian.hh"
 #include "exampleConfig.h"
 #include "example.h"
 #include "lacze_do_gnuplota.hh"
 
-#define DL_BOKU   50
-
 using namespace std;
 
-
-/*!
- * Przyklad zapisu wspolrzednych zbioru punktow do strumienia wyjściowego.
- * Dane sa odpowiednio sformatowane, tzn. przyjęto notację stałoprzecinkową
- * z dokładnością do 10 miejsca po przecinku. Szerokość wyświetlanego pola 
- * to 16 miejsc, sposób wyrównywania - do prawej strony.
- * \param[in] StrmWy - strumien wyjsciowy, do ktorego maja zostac zapisane
- *                     kolejne wspolrzedne.
- * \param[in] Przesuniecie - ten parameter jest tylko po to, aby pokazać
- *                          mozliwosc zmiany wspolrzednych i prostokata
- *                          i zmiane jego polorzenia na okienku graficznym
- *                         rysownym przez gnuplota.
- * \retval true - gdy operacja zapisu powiodła się,
- * \retval false - w przypadku przeciwnym.
- */
-void PrzykladZapisuWspolrzednychDoStrumienia( ostream&     StrmWy, 
-                                              double       Przesuniecie
-                                            )
+void Menu_wyswietl()
 {
-   //---------------------------------------------------------------
-   // To tylko przyklad !!!
-   // W programie nalezy uzywać pojęcia wektora, a nie oddzielnych 
-   // zmiennych do reprezentowania wspolrzednych!
-   //
-  double  x1, y1, z1;
-
-  x1 = y1 = z1 = Przesuniecie;
-
-  StrmWy << setw(16) << fixed << setprecision(10) << x1
-         << setw(16) << fixed << setprecision(10) << y1
-         << setw(16) << fixed << setprecision(10) << z1 
-         << endl;
-
-  StrmWy << setw(16) << fixed << setprecision(10) << x1+DL_BOKU
-         << setw(16) << fixed << setprecision(10) << y1
-         << setw(16) << fixed << setprecision(10) << z1 
-         << endl;
-
-  StrmWy << endl;
-
-  StrmWy << setw(16) << fixed << setprecision(10) << x1
-         << setw(16) << fixed << setprecision(10) << y1+DL_BOKU
-         << setw(16) << fixed << setprecision(10) << z1 
-         << endl;
-
-  StrmWy << setw(16) << fixed << setprecision(10) << x1+DL_BOKU
-         << setw(16) << fixed << setprecision(10) << y1+DL_BOKU
-         << setw(16) << fixed << setprecision(10) << z1 
-         << endl;
-
-  StrmWy << endl;
-
-  StrmWy << setw(16) << fixed << setprecision(10) << x1
-         << setw(16) << fixed << setprecision(10) << y1+DL_BOKU
-         << setw(16) << fixed << setprecision(10) << z1+DL_BOKU 
-         << endl;
-
-  StrmWy << setw(16) << fixed << setprecision(10) << x1+DL_BOKU
-         << setw(16) << fixed << setprecision(10) << y1+DL_BOKU
-         << setw(16) << fixed << setprecision(10) << z1+DL_BOKU 
-         << endl;
-
-  StrmWy << endl;
-
-  StrmWy << setw(16) << fixed << setprecision(10) << x1
-         << setw(16) << fixed << setprecision(10) << y1
-         << setw(16) << fixed << setprecision(10) << z1+DL_BOKU 
-         << endl;
-
-  StrmWy << setw(16) << fixed << setprecision(10) << x1+DL_BOKU
-         << setw(16) << fixed << setprecision(10) << y1
-         << setw(16) << fixed << setprecision(10) << z1+DL_BOKU 
-         << endl;
-
-  StrmWy << endl;
- 
-  StrmWy << setw(16) << fixed << setprecision(10) << x1
-         << setw(16) << fixed << setprecision(10) << y1
-         << setw(16) << fixed << setprecision(10) << z1 
-         << endl;
-
-  StrmWy << setw(16) << fixed << setprecision(10) << x1+DL_BOKU
-         << setw(16) << fixed << setprecision(10) << y1
-         << setw(16) << fixed << setprecision(10) << z1 
-         << endl;
-                             // Jeszcze raz zapisujemy pierwsze dwa wierzcholki,
-                             // aby gnuplot narysowal zamkniętą powierzchnie.
+  cout<<endl;
+  cout<<" Menu programu:"<<endl<<endl;
+  cout<<" o - obrot prostopadloscianu o zadany kat"<<endl;
+  cout<<" p - przesuniecie prostopadloscianu o zadany wektor"<<endl;
+  cout<<" w - wyswietlanie wspolrzednych wierzcholkow"<<endl;
+  cout<<" m - wyswietl menu"<<endl;
+  cout<<" k - koniec dzialania programu"<<endl;
 }
 
-
-/*!
- * Przyklad zapisu wspolrzednych zbioru punktow do pliku, z ktorego
- * dane odczyta program gnuplot i narysuje je w swoim oknie graficznym.
- * \param[in] sNazwaPliku - nazwa pliku, do którego zostana zapisane
- *                          wspolrzędne punktów.
- * \param[in] Przesuniecie - ten parameter jest tylko po to, aby pokazać
- *                          mozliwosc zmiany wspolrzednych i prostokata
- *                          i zmiane jego polorzenia na okienku graficznym
- *                         rysownym przez gnuplota.
- * \retval true - gdy operacja zapisu powiodła się,
- * \retval false - w przypadku przeciwnym.
- */
-bool PrzykladZapisuWspolrzednychDoPliku( const char  *sNazwaPliku,
-                                         double       Przesuniecie
-                                       )
-{
-  ofstream  StrmPlikowy;
-
-  StrmPlikowy.open(sNazwaPliku);
-  if (!StrmPlikowy.is_open())  {
-    cerr << ":(  Operacja otwarcia do zapisu \"" << sNazwaPliku << "\"" << endl
-	 << ":(  nie powiodla sie." << endl;
-    return false;
-  }
-
-  PrzykladZapisuWspolrzednychDoStrumienia(StrmPlikowy,Przesuniecie);
-
-  StrmPlikowy.close();
-  return !StrmPlikowy.fail();
-}
 
 
 /*
@@ -149,55 +41,109 @@ bool PrzykladZapisuWspolrzednychDoPliku( const char  *sNazwaPliku,
  * CMake definitions (here the version number) from source code.
  */
 int main() {
-  cout << "C++ Boiler Plate v"
-            << PROJECT_VERSION_MAJOR
+  cout << "Project Rotation 3D based on C++ Boiler Plate v"
+            << PROJECT_VERSION_MAJOR /*duże zmiany, najczęściej brak kompatybilności wstecz */
             << "."
-            << PROJECT_VERSION_MINOR
+            << PROJECT_VERSION_MINOR /* istotne zmiany */
             << "."
-            << PROJECT_VERSION_PATCH
+            << PROJECT_VERSION_PATCH /* naprawianie bugów */
             << "."
-            << PROJECT_VERSION_TWEAK
+            << PROJECT_VERSION_TWEAK /* zmiany estetyczne itd. */
             << endl;
 
-  //Prostopadloscian             Pr;   // To tylko przykladowe definicje zmiennej
-  PzG::LaczeDoGNUPlota  Lacze;  // Ta zmienna jest potrzebna do wizualizacji
-                                // rysunku prostokata
+  PzG::LaczeDoGNUPlota  Lacze;  // Zmienna odpowiada za wizualizacje rysunku prostopadloscianu
+  // Zmienna odpowiada za zapis wspolrzednych do strumienia
+  /*Prostopadloscian Pr{{10,  25,  10 }
+                      {150, 25,  10 },
+                      {10,  100, 10 },
+                      {150, 100, 10 },
+                      {10,  100, 110},
+                      {150, 100, 110},
+                      {10,  25,  110},
+                      {150, 25,  110}};  */
+ Prostopadloscian Pr{{10,  25,  10 },{150, 25,  10 },{10,  100, 10 },{150, 100, 10 },{10,  100, 110},{150, 100, 110},{10,  25,  110},{150, 25,  110}};  
 
-
-   //-------------------------------------------------------
-   //  Wspolrzedne wierzcholkow beda zapisywane w pliku "prostokat.dat"
-   //  Ponizsze metody powoduja, ze dane z pliku beda wizualizowane
-   //  na dwa sposoby:
-   //   1. Rysowane jako linia ciagl o grubosci 2 piksele
-   //
-  Lacze.DodajNazwePliku("prostopadloscian.dat",PzG::RR_Ciagly,2);
-   //
-   //  Ustawienie trybu rysowania 3D, tzn. rysowany zbiór punktów
-   //  znajduje się na wspólnej płaszczyźnie. Z tego powodu powoduj
-   //  jako wspolrzedne punktow podajemy x,y,z.
-   //
+  Lacze.DodajNazwePliku("../datasets/Prostopadloscian.dat",PzG::RR_Ciagly,2);
   Lacze.ZmienTrybRys(PzG::TR_3D);
-
-   // Ustawienie zakresow poszczegolnych osi
+  // Ustawienie zakresow poszczegolnych osi
   Lacze.UstawZakresY(-155,155);
   Lacze.UstawZakresX(-155,155);
   Lacze.UstawZakresZ(-155,155);
 
-  
-  PrzykladZapisuWspolrzednychDoStrumienia(cout,0);
-  if (!PrzykladZapisuWspolrzednychDoPliku("prostopadloscian.dat",0)) return 1;
-  Lacze.Rysuj(); // <- Tutaj gnuplot rysuje, to co zapisaliśmy do pliku
-  cout << "Naciśnij ENTER, aby kontynuowac" << endl;
-  cin.ignore(10000,'\n');
+  double kat;
+  int krotnosc;
+  Wektor3D wektor;
+  Prostopadloscian prosty;
+  char opcja;  //zmienna dla obslugi prostego menu
 
-   //----------------------------------------------------------
-   // Ponownie wypisuje wspolrzedne i rysuje prostokąt w innym miejscu.
-   //
-  PrzykladZapisuWspolrzednychDoStrumienia(cout,50);
-  if (!PrzykladZapisuWspolrzednychDoPliku("prostopadloscian.dat",50)) return 1;
-  Lacze.Rysuj(); // <- Tutaj gnuplot rysuje, to co zapisaliśmy do pliku
-  cout << "Naciśnij ENTER, aby kontynuowac" << endl;
-  cin.ignore(10000,'\n');
+  cout<<"\n Program rysuje w gnuplocie Prostopadloscian z pliku,"<<endl;
+  cout<<" obraca go o zadany kat i przesuwa o zadany wektor."<<endl;
+  cout<<" Wynik jest wyswietlany w gnuplocie oraz zapisywany do pliku."<<endl;
+  
+  Menu_wyswietl();   //wyswietlenie prostego menu
+
+  while(opcja!='k')
+  {
+    if(!prosty.Zapis_do_pliku("../datasets/Prostopadloscian.dat",Pr))
+    {
+      cerr<<"Blad! Nie udalo sie wczytac pliku ze wspolrzednymi prostokota!"<<endl;
+      return 1;
+    }
+    prosty.Boki(); // wyswietla dlugosci bokow z dokladnoscia do 10 miejsc po przecinku
+    Lacze.Rysuj(); // <- Tutaj gnuplot rysuje, to co zapisaliśmy do pliku
+    cout<<endl;
+    cout<<" Twoj wybor? (m - menu) > ";
+    cin>>opcja;
+    cout<<endl;
+
+    switch(opcja)   //obsluga prostego menu
+    {
+      case 'o':
+      {
+        cout<<" Podaj wartosc kata obrotu w stopniach"<<endl;
+        cin>>kat;
+        cout<<" Ile razy operacja obrotu ma byc powtorzona?"<<endl;
+        cin>>krotnosc;
+        prosty.Obrot(kat,krotnosc);
+        prosty.Zapis_do_pliku("../datasets/Prostopadloscian.dat",Pr);
+        Lacze.Rysuj();
+        prosty.Boki();
+        break;
+      }
+      case 'p':
+      {
+        cout<<" Wprowadz wspolrzedne wektora translacji w postaci dwoch liczb"<<endl;
+        cout<<" tzn. wspolrzednej x oraz wsporzednej y."<<endl;
+        cin>>wektor;
+        prosty.Przesuniecie(wektor);
+        prosty.Zapis_do_pliku("../datasets/Prostopadloscian.dat",Pr);
+        Lacze.Rysuj();
+        break;
+      }
+      case 'w':
+      {
+        cout<<endl;
+        cout<<prosty<<endl;     //wyswietla nam wspolrzedne wierzcholkow
+        break;
+      }
+      case 'm':
+      {
+        Menu_wyswietl(); 
+        break;
+      }
+      case 'k':
+      {
+        cout<<"\n Koniec dzialania programu.\n"<<endl;
+        break;
+      }
+      default:
+      {
+        cerr<<"\n Blad! Nie rozpoznano opcji menu. Sprobuj jeszcze raz."<<endl;
+        break;
+      }
+    }
+  }   
+
 
   // Bring in the dummy class from the example source,
   // just to show that it is accessible from main.cpp.
